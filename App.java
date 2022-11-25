59dec4016e5e30bdbc01056813bc0f43231886d0
bilaly <billycsk@gmail.com>
2022-11-25 21:14:02 +0100

    Rajout de la partie orienté objet

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        // System.out.println("Hello word");
+        Chien cleps1 = new Chien("Chakaplus", 4, 1.70);
+    }
+}
---
+++ Chien.java
@@ -0,0 +1,14 @@
+public class Chien {
+    private String name;
+    private int age;
+    private double taillePoile;
+
+    public Chien(String name, int age, double taillePoile) {
+        this.taillePoile = taillePoile;
+        this.name = name;
+        this.age = age;
+        System.out.println(
+                "Je mappelle" + " " + this.name + " " + "j'ai" + " " + this.age + "ans et mes poils font :" + " "
+                        + this.taillePoile + " " + "cm");
+    }
+}
