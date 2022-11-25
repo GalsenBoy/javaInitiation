98f6b374407ab7a6e481d73427d454e12519b2df
bilaly <billycsk@gmail.com>
2022-11-25 16:21:16 +0100

    First Project java

---
+++ Cylindre.java
@@ -0,0 +1,11 @@
+public class Cylindre {
+    static double aireVolume(double rayon,double hauteur){
+        double aire = rayon* rayon * 3.14;
+        System.out.println("L'air vaut" + " " + aire);
+        double volume = aire * hauteur;
+        return volume;
+    }
+    public static void main(String[] args){
+        System.out.println("La volume vaut" + " " + aireVolume(5,15));
+    }
+}
---
+++ Multiple.java
@@ -0,0 +1,12 @@
+public class Multiple {
+    static void mulipleDe(int number){
+        if (number % 5 == 0 ){
+            System.out.println(number + " " + "est un multiple de 5");
+        }
+        //if (number)
+    }
+    public static void main (String[] args){
+        int y = 1998;
+        System.out.println(y.indexOf(1));
+    }
+}
