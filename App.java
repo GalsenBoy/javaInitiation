509422f977f1578d499a070a7eb5cbdf997d4cf6
bilaly <billycsk@gmail.com>
2022-11-29 20:22:44 +0100

    Rajout méthode affichage dans la Class Chien

--- App.java
+++ App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) {
         // System.out.println("Hello word");
         Chien cleps1 = new Chien("Chakaplus", 4, 1.70);
+        // Chien cleps2 = new Chien();
+        cleps1.afficheInfo();
+        System.out.println(cleps1.getAge());
     }
 }
--- Chien.java
+++ Chien.java
@@ -7,8 +7,19 @@ public class Chien {
         this.taillePoile = taillePoile;
         this.name = name;
         this.age = age;
+    }
+
+    public void afficheInfo() {
         System.out.println(
                 "Je mappelle" + " " + this.name + " " + "j'ai" + " " + this.age + "ans et mes poils font :" + " "
                         + this.taillePoile + " " + "cm");
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
