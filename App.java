319195fc4251b0efb7e4b46e3c618ec2260ab21b
bilaly <billycsk@gmail.com>
2022-12-03 13:51:14 +0100

    Rajout d'exception

--- App.java
+++ App.java
@@ -4,10 +4,8 @@ public class App {
         // Toutou cleps2 = new Toutou();
         cleps1.afficheInfo();
         System.out.println(cleps1.getAge());
-
         Toutou cleps2 = new Toutou("felix", 2);
         cleps2.afficheInfo();
-
         Toutou cleps3 = new Toutou("jetki");
         cleps3.afficheInfo();
     }
